beba52347fe7db20ad3bcb5ad895eb8b77dd5d09
ohhyogeun <ohhyogeun@khu.ac.kr>
2019-09-21 10:20:56 +0900

    190921 10:20

--- titanic/controller.py
+++ titanic/controller.py
@@ -1,22 +1,23 @@
 from titanic.model import TitanicModel
 import pandas as pd
 import numpy as np
-
 class TitanicController:
     def __init__(self):
-        self.m = TitanicModel()
+        self._m = TitanicModel()
         self._context = './data/'
         self._train = self.create_train()
 
-    def create_train(self) -> object:
-        m = self.m
+    def create_train(self)->object:
+        print('0 >>>> ')
+        m = self._m
+        print('1 >>>> ' + self._context)
         m.context = self._context
+        print('2 >>>> '+m.context)
         m.fname = 'train.csv'
         t1 = m.new_dfame()
 
         m.fname = 'test.csv'
         t2 = m.new_dfame()
 
-        train = m.hook_process(t1, t2)
-
+        return m.hook_process(t1, t2)
 
